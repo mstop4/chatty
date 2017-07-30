ef404f3fad78b2520b2b206ca62f6b48e0b1f871
mstop4 <mstop4@gmail.com>
2017-07-30 19:26:41 +0000

    added dotenv

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 var config = require('./webpack.config')
 
 // Set the host and port according to env file
-var HOST = '0.0.0.0'
-var PORT = 3002
+var HOST = process.env.HOST || '0.0.0.0'
+var PORT = process.env.PORT || 3001
 
 // Create a new server
 new WebpackDevServer(webpack(config), {
